e4c08b4d29f16be596de66fafba10a164405f4ed
Omadbek Teshaboev <oteshaboyev8@gmail.com>
2023-06-23 16:29:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-ACCOUNT_EMAIL = "marhamatitcenteromadbek2003@gmail.com"
-ACCOUNT_PASSWORD = "vF7S&ZYHV%ak2Rg"
+ACCOUNT_EMAIL = "EMAIL ACCOUNT"
+ACCOUNT_PASSWORD = "ACCOUNT PASSWORD"
 
 # chromedriver_autoinstaller.install()
 chrome_driver_path = "C:\Development\chromedriver.exe"
